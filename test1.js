afa97d9fd9580704dedb1fb93973a31a42c8b216
Adam Hay <heyadam66@gmail.com>
2019-10-10 14:03:11 -0500

    Did a few more functions

--- test1.js
+++ test1.js
@@ -4,3 +4,18 @@
 function addition(num) {
     return ++num
 }
+
+//Create a function that takes two numbers as 
+//arguments and return their sum.
+
+function addition(a, b) {
+    return a + b;
+}
+
+
+//Write a function that takes an integer minutes 
+//and converts it to seconds.
+
+function convert(minutes) {
+    return minutes * 60
+}
