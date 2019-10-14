d577e3301607e1504185b85f7fa959bba8ae9667
Adam Hay <heyadam66@gmail.com>
2019-10-14 15:16:22 -0500

    added another test

--- test1.js
+++ test1.js
@@ -19,3 +19,12 @@ function addition(a, b) {
 function convert(minutes) {
     return minutes * 60
 }
+
+
+//There is a single operator in JavaScript, capable of providing 
+//the remainder of a division operation. Two numbers are passed as parameters. The 
+//first parameter divided by the second parameter will have a remainder, possibly zero. Return that value.
+
+function remainder(x, y) {
+    return x % y
+}
